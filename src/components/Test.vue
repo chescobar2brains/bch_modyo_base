0a2cb2dc983c5eddb9a739782b3ea2d36b3c6182
Christian Escobar F <44533046+chescobarf@users.noreply.github.com>
2023-01-04 15:21:03 -0300

    Revert "Feat: Add Inputs"
    This reverts commit 5eea812b5dfd1fdc9e615d14999d09ce9a89bc93.

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="container mx-auto mt-5 text-center">
-    <div class="w-52">
-      <vInputComponent
-        label="Componente de texto generico"
-        errormsg="Ingresa texto"
-        color="#80bdff"
-        class="rounded"
-      />
+  <div class="bg-primary-ultra_dark">
+    <div
+      class="text-backup-yellow flex gap-2 items-center justify-center text-xl bg-brand-dark"
+    >
+      <span class="icos-pagos-masivos"></span>
+      <p class="">Testing Minerva</p>
     </div>
   </div>
 </template>
 
-<script></script>
+<script setup>
+import API from "@/api/newModyoAPI";
+
+const newApi = API("personas", "nuevo-beneficio");
+
+try {
+  newApi.get();
+} catch (error) {}
+</script>
+
+<style lang="scss" scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,5 @@
-const { defineConfig } = require('@vue/cli-service');
-const { VuetifyPlugin } = require("webpack-plugin-vuetify");
+const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  configureWebpack: {
-    plugins: [
-      new VuetifyPlugin({
-        styles: { configFile: "src/settings.scss" },
-      }),
-    ],
-  },
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableLegacy: false,
-      runtimeOnly: false,
-      compositionOnly: false,
-      fullInstall: true
-    }
-  }
-});
+})
